dd9ac25ccd782b12f382acb017f07de8a03e90e7
Shubham Singh <shubham.singh@mutualmobile.com>
2022-08-20 12:22:28 +0530

    1. Asking for Card component params now 2. Added demo gif

--- app/src/main/java/com/shubham/compose3dcard/ui/components/Card3D.kt
+++ app/src/main/java/com/shubham/compose3dcard/ui/components/Card3D.kt
@@ -2,10 +2,12 @@ package com.shubham.compose3dcard.ui.components
 
 import android.content.res.Configuration
 import android.graphics.Paint
+import androidx.annotation.FloatRange
 import androidx.compose.animation.core.animateOffsetAsState
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -33,30 +35,73 @@ import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.LayoutCoordinates
 import androidx.compose.ui.layout.onPlaced
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import com.shubham.compose3dcard.ui.theme.Compose3DCardTheme
 
-val CardRoundedCornerSize = 16.dp
-
-// Since we want to deal with smaller angles (i.e. between -20 to 20 degrees), therefore we'll divide the incoming value by this factor.
-const val OffsetSlowDownFactor = 30f
+// Since we want to deal with smaller angles, therefore we'll divide the incoming value by this factor. You can increase or decrease the value to your liking.
+private const val OffsetSlowDownFactor = 30f
+private const val CardAspectRatio = 1.75f
+private val CardRoundedCornerSize = 16.dp
 
 @Composable
-fun Card3D() {
-    Box(contentAlignment = Alignment.Center) {
-        BackgroundGlowSurface()
-        ForegroundClickableSurface()
+fun Card3D(
+    modifier: Modifier = Modifier,
+    glowOffset: Offset = Offset.Zero,
+    glowRadius: Float = 16f,
+    glowRoundedCornerSize: Dp = CardRoundedCornerSize,
+    gradientColors: List<Color> = listOf(
+        Color.Magenta,
+        Color.Red,
+        Color.Blue,
+        Color.Green,
+        Color.Yellow,
+        Color.Cyan,
+    ),
+    glowColor: Color = Color.Green,
+    animationMaxAngle: Float = 20f,
+    offsetSlowDownFactor: Float = OffsetSlowDownFactor,
+    @FloatRange(from = 1.0)
+    cardAspectRatio: Float = CardAspectRatio,
+) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        val commonModifier = remember {
+            Modifier
+                .fillMaxWidth()
+                .aspectRatio(cardAspectRatio)
+                .padding(20.dp)
+        }
+        BackgroundGlowSurface(
+            modifier = commonModifier,
+            glowOffset = glowOffset,
+            glowRadius = glowRadius,
+            glowRoundedCornerSize = glowRoundedCornerSize,
+            gradientColors = gradientColors,
+            glowColor = glowColor,
+        )
+        ForegroundClickableSurface(
+            modifier = commonModifier,
+            maxAngle = animationMaxAngle,
+            offsetSlowDownFactor = offsetSlowDownFactor,
+        )
     }
 }
 
 @Composable
-private fun BackgroundGlowSurface() {
-    Surface(
-        modifier = Modifier
-            .fillMaxWidth()
-            .aspectRatio(2f)
-            .padding(20.dp)
+private fun BackgroundGlowSurface(
+    modifier: Modifier = Modifier,
+    glowOffset: Offset,
+    glowRadius: Float,
+    glowRoundedCornerSize: Dp,
+    gradientColors: List<Color>,
+    glowColor: Color,
+) {
+    Spacer(
+        modifier = modifier
             .drawBehind {
                 drawIntoCanvas { canvas ->
                     canvas.nativeCanvas.drawRoundRect(
@@ -64,60 +109,58 @@ private fun BackgroundGlowSurface() {
                         0f,
                         size.width,
                         size.height,
-                        CardRoundedCornerSize.toPx(),
-                        CardRoundedCornerSize.toPx(),
+                        glowRoundedCornerSize.toPx(),
+                        glowRoundedCornerSize.toPx(),
                         Paint().apply {
                             shader = SweepGradientShader(
-                                colors = listOf(
-                                    Color.Magenta,
-                                    Color.Red,
-                                    Color.Blue,
-                                    Color.Green,
-                                    Color.Yellow,
-                                    Color.Cyan,
-                                ),
+                                colors = gradientColors,
                                 center = size.center
                             )
                             setShadowLayer(
-                                8f,
-                                0f,
-                                0f,
-                                Color.Green.toArgb()
+                                glowRadius,
+                                glowOffset.x,
+                                glowOffset.y,
+                                glowColor.toArgb()
                             )
                         }
                     )
                 }
             },
-        color = Color.Transparent,
-        content = {}
     )
 }
 
 @Composable
-private fun ForegroundClickableSurface() {
+private fun ForegroundClickableSurface(
+    modifier: Modifier = Modifier,
+    maxAngle: Float = 20f,
+    offsetSlowDownFactor: Float,
+) {
     var cardCenterOffset: Offset? by remember { mutableStateOf(null) }
     var currentOffset: Offset by remember { mutableStateOf(Offset.Zero) }
     val animatedOffset by animateOffsetAsState(targetValue = currentOffset)
 
     Surface(
-        modifier = Modifier
-            .fillMaxSize()
-            .aspectRatio(2f)
+        modifier = modifier
+            .padding(2.dp)
             .zIndex(1f)
-            .padding(22.dp)
             .onPlaced { coordinates -> cardCenterOffset = coordinates.centerOffset() }
             .graphicsLayer {
                 rotationX =
                     -animatedOffset.y // Because Modifier.pointerInput gives Offset(y,x) instead of Offset(x,y)
                         .times(2)
-                        .limitAngleValue()
+                        .limitAngleValue(maxValue = maxAngle)
 
-                rotationY = animatedOffset.x.limitAngleValue()
+                rotationY = animatedOffset.x
+                    .div(2)
+                    .limitAngleValue(maxValue = maxAngle)
             }
             .pointerInput(Unit) {
                 detectTapGestures(
                     onPress = { offset ->
-                        currentOffset = offset.centerOffsetAdjustedValue(cardCenterOffset)
+                        currentOffset = offset.centerOffsetAdjustedValue(
+                            centerOffset = cardCenterOffset,
+                            slowDownFactor = offsetSlowDownFactor
+                        )
 
                         awaitRelease()
 
@@ -126,7 +169,10 @@ private fun ForegroundClickableSurface() {
                         detectDragGestures(
                             onDrag = { change, _ ->
                                 currentOffset =
-                                    change.position.centerOffsetAdjustedValue(cardCenterOffset)
+                                    change.position.centerOffsetAdjustedValue(
+                                        centerOffset = cardCenterOffset,
+                                        slowDownFactor = offsetSlowDownFactor
+                                    )
                             },
                             onDragStart = { dragStartOffset ->
                                 currentOffset = dragStartOffset
@@ -154,8 +200,10 @@ private fun Float.limitAngleValue(maxValue: Float = 20f) =
 
 private fun LayoutCoordinates.centerOffset() = Offset(x = size.width / 2f, y = size.height / 2f)
 
-private fun Offset.centerOffsetAdjustedValue(centerOffset: Offset?) =
-    (this - centerOffset!!) / OffsetSlowDownFactor
+private fun Offset.centerOffsetAdjustedValue(
+    centerOffset: Offset?,
+    slowDownFactor: Float,
+) = (this - centerOffset!!) / slowDownFactor
 
 @Preview(showSystemUi = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Preview(showSystemUi = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
